fd62a44a6fab3b89642082ce99b776e0cc397843
Josha Bartsch <jaybraker1@gmail.com>
2021-11-02 14:24:33 +0100

    Removed excess blank line

--- Test.java
+++ Test.java
@@ -7,7 +7,7 @@ public class Test
     public static void main(String[] args)
     {
         Bruch bruch1 = new Bruch(23, 3);
-        
+
         bruch1.setBruchdarstellung(new DoubleFormat());
         System.out.println(bruch1 + "\n");
 

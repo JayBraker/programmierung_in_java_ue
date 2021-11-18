545317eaedadc3e71a246fecee02d8a0681ad98b
Josha Bartsch <jaybraker1@gmail.com>
2021-11-18 10:19:13 +0100

    Basic classes added

---
+++ LinieTest.java
@@ -0,0 +1,24 @@
+package uebung7;
+
+import java.awt.Point;
+
+public class LinieTest
+{
+    public static void main(String[] args)
+    {
+
+        // neues Grafikfenster mit einer Zeichenflaeche der
+        // angegebenen Breite und Hoehe oeffnen:
+        Grafik g = new Grafik(300, 300);
+
+        // eine Linie zeichnen, die vom angegebenen Startpunkt bis
+        // zum angegebenen Endpunkt reicht:
+        Point startpunkt = new Point(10, 10);
+        Point endpunkt = new Point(200, 100);
+        Linie l = new Linie(startpunkt, endpunkt);
+        // => Der Koordinatenursprung liegt links oben!
+
+        g.add(l); // die Linie in die Zeichenflaeche einfuegen
+        g.aktualisiere(0); // die Zeichenflaeche sofort aktualisieren
+    }
+}

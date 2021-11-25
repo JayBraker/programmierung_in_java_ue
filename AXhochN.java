48a1ead7825d2be5b37aaf2d6ff271e938d3bd8b
Josha Bartsch <jaybraker1@gmail.com>
2021-11-25 10:39:00 +0100

    Initialize AXhochN

---
+++ AXhochN.java
@@ -0,0 +1,22 @@
+package uebung8;
+
+public class AXhochN implements Funktion {
+
+  private final double A;
+  private final double N;
+
+  public AXhochN(double a, double n) {
+    A = a;
+    N = n;
+  }
+
+  @Override
+  public double getY(double d) {
+    return A*Math.pow(d, N);
+  }
+
+  @Override
+  public Funktion getAbleitung() {
+    return new AXhochN(A*N, N-1);
+  }
+}

62ed525966d48e14191aad1b35a0d6e25eb85a0b
Josha Bartsch <jaybraker1@gmail.com>
2021-11-25 10:43:10 +0100

    Initialize Funktion Summe

---
+++ Summe.java
@@ -0,0 +1,22 @@
+package uebung8;
+
+public class Summe implements Funktion {
+
+  private final Funktion F1;
+  private final Funktion F2;
+
+  public Summe(Funktion f1, Funktion f2) {
+    F1 = f1;
+    F2 = f2;
+  }
+  
+  @Override
+  public double getY(double d) {
+    return F1.getY(d) + F2.getY(d);
+  }
+
+  @Override
+  public Funktion getAbleitung() {
+    return new Summe(F1.getAbleitung(), F2.getAbleitung());
+  }
+}
